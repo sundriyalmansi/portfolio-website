46c1225847ac4373cf63ea350594be8d99b6bd03
MansiSundriyal <sundriyalmansi@gmail.com>
2023-08-14 15:39:23 -0500

    Add Header Component and Typed.js

---
+++ src/components/About/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="aboutmain" id="aboutpage">
+            <div className="container">
+                <div className="aboutme">
+                    <div className="row">  
+                        <h2 className="title">About me</h2>
+                    </div>
+                <p className='subTitle'>Trying to create better use experiences by learning something new every day.</p>
+                <div className="row">
+                    <div className="col-12">
+                        <p className="aboutmetext">Hi, i’m Mansi Sundriyal. I’m software developer with inclination towards front-end development and passion for User Experience design. I have an eye for innovative & creative problem solving designs and developing amazing user experiences.</p>
+                        <br></br>
+                        
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
